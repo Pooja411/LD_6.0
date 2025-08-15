b72cb0ef0f09ea57c043c71c78cdf33c9c623a80
Pooja <151989825+Pooja411@users.noreply.github.com>
2025-08-15 10:47:40 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# -----------------------
 # CONFIG
-# -----------------------
 BACKEND_URL="http://127.0.0.1:8000"
 USERNAME="$1"   # Pass username as first argument: ./start.sh player1
 
@@ -11,9 +9,7 @@ if [ -z "$USERNAME" ]; then
     exit 1
 fi
 
-# -----------------------
 # GET CURRENT PROGRESS
-# -----------------------
 RESPONSE=$(curl -s "$BACKEND_URL/progress/$USERNAME")
 
 CURRENT_LEVEL=$(echo "$RESPONSE" | grep -oP '"current_level":\K[0-9]+')
@@ -25,8 +21,7 @@ fi
 
 echo "Welcome, $USERNAME! You are currently on Level $CURRENT_LEVEL."
 
-# -----------------------
+
 # START CONTAINER FOR THIS LEVEL
-# -----------------------
 echo "Starting challenge container for Level $CURRENT_LEVEL..."
 docker run -it --rm "ctf_level_$CURRENT_LEVEL"
